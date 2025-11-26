cb61e22ea680e1346be2dc6e76ab38da6fba67e7
Kevin Green <Greenkevin28@icloud.com>
2025-11-26 10:25:17 -0330

    Set up connection to database in DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,4 +1,33 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
 
+    // Never wanted to include actual password,useranme in public repo, can message you them privately but
+    // dont think you'll need them
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "Username";
+    private static final String PASSWORD = "password";
+
+    public static Connection getCon() {
+
+        Connection connection = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (ClassNotFoundException e) {
+
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
